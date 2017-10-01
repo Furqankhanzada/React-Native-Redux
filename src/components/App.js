d3435252ed1480d6d911966b56ae83877ea911e8
Muhammad Furqan <furqan.khanzada@gmail.com>
2017-10-01 22:36:25 +0500

    user photos and popular photos implemented

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux'
 import { getUser } from 'ReactNativeRedux/src/actions/user';
+import { getUserPhotos, getPopularPhotos } from 'ReactNativeRedux/src/actions/photo';
 
 class App extends Component {
     componentWillMount(){
         this.props.dispatch(getUser());
+        this.props.dispatch(getUserPhotos());
+        this.props.dispatch(getPopularPhotos());
     }
     render() {
         console.log('this.props: ', this.props)
@@ -24,8 +27,8 @@ class App extends Component {
 }
 
 const mapStateToProps = state => {
-    let { user } = state;
-    return { user }
+    let { user, userPhotos, popularPhotos } = state;
+    return { user, userPhotos, popularPhotos }
 };
 
 export default connect(mapStateToProps)(App);
