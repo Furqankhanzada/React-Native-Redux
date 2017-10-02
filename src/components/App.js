624e50160d4f2ae6fb327ef20682b9c52abd4fc3
Muhammad Furqan <furqan.khanzada@gmail.com>
2017-10-02 20:32:33 +0500

    user info and slider implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { connect } from 'react-redux'
 import { getUser } from 'ReactNativeRedux/src/actions/user';
 import { getUserPhotos, getPopularPhotos } from 'ReactNativeRedux/src/actions/photo';
+
+import UserInfo from 'ReactNativeRedux/src/components/userInfo/UserInfo'
+import PhotoSlider from 'ReactNativeRedux/src/components/photoSlider/PhotoSlider'
 
 class App extends Component {
     componentWillMount(){
@@ -13,14 +16,9 @@ class App extends Component {
     render() {
         console.log('this.props: ', this.props)
         return (
-            <View>
-                <Text>Testing ...</Text>
-                <Text>Testing ...</Text>
-                <Text>Testing ...</Text>
-                <Text>Testing ...</Text>
-                <Text>Testing ...</Text>
-                <Text>Testing ...</Text>
-                <Text>Testing ...</Text>
+            <View style={AppStyles.container}>
+                <UserInfo user={this.props.user} />
+                {!this.props.userPhotos.loading ? <PhotoSlider userPhotos={this.props.userPhotos} /> : null}
             </View>
         );
     }
@@ -32,3 +30,12 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps)(App);
+
+
+export const AppStyles = StyleSheet.create({
+    container:{
+        padding: 10,
+        marginBottom: 15,
+        flex: 1
+    }
+});
---
+++ src/components/photoSlider/PhotoSlider.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
+import { PhotoSliderStyles } from 'ReactNativeRedux/src/components/photoSlider/PhotoSliderStyles'
+import Swiper from 'react-native-swiper'
+
+export default  class PhotoSlider extends Component {
+    render() {
+        const { userPhotos } = this.props;
+        console.log('posts : ', this.props)
+
+        if(userPhotos.loading || !userPhotos.posts )
+            return null;
+
+        return(
+            <Swiper style={ PhotoSliderStyles.container } dot={<View style={ PhotoSliderStyles.dot } />}
+                    activeDot={<View style={ PhotoSliderStyles.activeDot } />} paginationStyle={ PhotoSliderStyles.paginationStyle }>
+                {userPhotos.posts.map((post) => {
+                    return <View key={post.objectId} style={ PhotoSliderStyles.slide }>
+                        <Image style={ PhotoSliderStyles.sliderImage } source={{ uri: post.thumbnail}} />
+                    </View>
+                })}
+            </Swiper>
+        );
+    }
+}
